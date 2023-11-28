a3d7c7ab3c601b84061714c426de5ca853f2280c
rishav bose <rishavbose@rishavs-MacBook-Pro.local>
2023-11-28 19:46:04 +0530

    Updates

--- vaux/src/context/AuthContext.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-
-export const AuthContext = createContext({
-  token: null,
-  setToken: () => {},
-});
--- vaux/src/environment/environment.ts
+++
@@ -1,8 +0,0 @@
-import { EnvironmentSchema } from './environment-schema';
-
-
-export const environment: EnvironmentSchema = {
-  production: false,
-  baseURL: 'https://731c-2405-201-9005-3f54-c197-aaab-db6f-882a.ngrok-free.app',
-  envName: 'DEVELOPMENT',
-};
